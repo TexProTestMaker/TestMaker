91d78a6bd9657478546cc9d26ad90efc7cfffc6b
Melkoten <muryginperm@gmail.com>
2025-03-20 14:55:56 +0300

    Added Activity class

---
+++ app/src/main/java/com/example/testmakerdev/Activity.java
@@ -0,0 +1,12 @@
+package com.example.testmakerdev;
+public class Activity {
+    String name;
+    String type;
+
+    public String getName() {
+        return name;
+    }
+    public String getType() {
+        return type;
+    }
+}
