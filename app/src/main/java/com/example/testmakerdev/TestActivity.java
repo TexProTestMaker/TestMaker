4da385bee576bd04738ff3ef32b0512af3bc6542
Melkoten <muryginperm@gmail.com>
2025-04-07 20:47:27 +0300

    Реализации функции setQuestion, правки в коде

--- app/src/main/java/com/example/testmakerdev/TestActivity.java
+++ app/src/main/java/com/example/testmakerdev/TestActivity.java
@@ -6,25 +6,70 @@ import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import java.util.List;
 
 public class TestActivity extends AppCompatActivity {
 
-    private TextView questionTextView;
+    private TextView questionTextView, testTitleTextView;
     private RadioGroup answersGroup;
     private Button checkAnswerButton, nextQuestionButton;
     private int currentQuestionIndex = 0;
+    private int counterCorrect = 0;
+    private Test currentTest;
+    private List<Question> allQuestions;
+    private Question currentQuestion;
+    private int length;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test);  // Подключаем layout
+        setContentView(R.layout.activity_test);
 
-        // Инициализация элементов
-        questionTextView = findViewById(R.id.questionText);
+        questionTextView = findViewById(R.id.questionText); //соединяем кнопки
         answersGroup = findViewById(R.id.answersGroup);
         checkAnswerButton = findViewById(R.id.checkAnswerButton);
         nextQuestionButton = findViewById(R.id.nextQuestionButton);
+        testTitleTextView = findViewById(R.id.testTitle);
+
+        //String filename = "filename";
+        //currentTest= new Test(filename); Так должен инициализироваться тест в финальной форме - по названию
+        //Здесь сделать прототип теста
+        allQuestions = currentTest.getQuestions();
+        length = allQuestions.size();
+
+        testTitleTextView.setText(currentTest.getName()); //заполняем название теста
+
+        setQuestion();
+
+        checkAnswerButton.setOnClickListener(v -> checkAnswer());
+        nextQuestionButton.setOnClickListener(v -> nextQuestion());
+    }
+    private void setQuestion() {
+        currentQuestion = allQuestions.get(currentQuestionIndex); //берём текущий вопрос из списка всех вопросов
+        questionTextView.setText(currentQuestion.getQuestionText()); //заполняем вопрос
+        for (int i = 0; i < answersGroup.getChildCount(); i++) { //заполняем кнопки текстом из вопроса
+            RadioButton radioButton = (RadioButton) answersGroup.getChildAt(i);
+            if (i < currentQuestion.getAnswers().size()) { //проверяем, что для этих кнопок есть варианты ответа
+                radioButton.setText(currentQuestion.getAnswers().get(i));
+                radioButton.setVisibility(View.VISIBLE);
+            } else {
+                radioButton.setVisibility(View.GONE);
+            }
+        }
+    }
+    private void checkAnswer() {
+
+    }
+
+    private void nextQuestion() {
+
+    }
+    private void exitTest() {
+
     }
 }
