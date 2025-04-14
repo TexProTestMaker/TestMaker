7b53a0df86ce7eeefb15a28baf4c66454f0d0988
Tihon99 <tihon_baikal_99@mail.ru>
2025-04-14 22:39:02 +0300

    Task36 add Binding

--- app/src/main/java/com/example/testmakerdev/MainActivity.java
+++ app/src/main/java/com/example/testmakerdev/MainActivity.java
@@ -8,22 +8,33 @@ import android.widget.Button;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.testmakerdev.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
-    //aboba
-    private Button button;
+
+    ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        button = findViewById(R.id.buttonSwap);
+        //EdgeToEdge.enable(this);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+
+
+
+
+
+        /*button = findViewById(R.id.buttonSwap);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, TestActivity.class);
                 startActivity(intent);
             }
-        });
+        });*/
 
     }
 }
