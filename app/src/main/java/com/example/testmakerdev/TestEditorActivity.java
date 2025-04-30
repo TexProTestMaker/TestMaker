4e6291963b80b7ac986d8d428590bc8242a3563b
Melkoten <muryginperm@gmail.com>
2025-04-30 19:05:58 +0300

    Соединение кнопок в TestEditorActivity

--- app/src/main/java/com/example/testmakerdev/TestEditorActivity.java
+++ app/src/main/java/com/example/testmakerdev/TestEditorActivity.java
@@ -1,6 +1,9 @@
 package com.example.testmakerdev;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class TestEditorActivity extends AppCompatActivity {
 
+   private EditText testTitleEditText, curQuestionEditText, answer1EditText, answer2EditText, answer3EditText, answer4EditText;
+
+   private Button exitEditorButton, addQuestionButton, changeTitleButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_test_editor);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+        testTitleEditText = findViewById(R.id.testTitleEditText);
+        curQuestionEditText = findViewById(R.id.curQuestionEditText);
+        answer1EditText = findViewById(R.id.answer1EditText);
+        answer2EditText = findViewById(R.id.answer2EditText);
+        answer3EditText = findViewById(R.id.answer3EditText);
+        answer4EditText = findViewById(R.id.answer4EditText);
+
+
     }
 }
