cb1545f4beb783449adbdf4ef38eb3d05fbc2c7e
Melkoten <muryginperm@gmail.com>
2025-04-30 19:26:58 +0300

    Реализована кнопка перехода в TestEditor

--- app/src/main/java/com/example/testmakerdev/MainMenuFragment.java
+++ app/src/main/java/com/example/testmakerdev/MainMenuFragment.java
@@ -1,12 +1,16 @@
 package com.example.testmakerdev;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,6 +19,7 @@ import android.view.ViewGroup;
  */
 public class MainMenuFragment extends Fragment {
 
+    private Button moveToTestEditor;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -60,5 +65,17 @@ public class MainMenuFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main_menu, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        moveToTestEditor = view.findViewById(R.id.toTestEditorActivity);
+        moveToTestEditor.setOnClickListener(v -> goToTestEditor());
+    }
+
+    private void goToTestEditor() {
+        Intent intent = new Intent(getActivity(), TestEditorActivity.class);
+        startActivity(intent);
     }
 }
