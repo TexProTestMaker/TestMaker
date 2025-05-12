016be9b69127564e8e46fac8d28b6fd21249ba41
Tihon99 <tihon_baikal_99@mail.ru>
2025-05-13 02:21:04 +0300

    task65 add ViewPager2 in MainActivity.java

--- app/src/main/java/com/example/testmakerdev/MainActivity.java
+++ app/src/main/java/com/example/testmakerdev/MainActivity.java
@@ -10,30 +10,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.testmakerdev.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
-    ActivityMainBinding binding;
+    private ViewPager2 viewPager;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //EdgeToEdge.enable(this);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        /*binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         replaceFragment(new MainMenuFragment());
 
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
 
             int itemID = item.getItemId();
-
-            if (itemID == R.id.lectures) {
-                replaceFragment(new LectureListFragment());
-
-            } else if (itemID == R.id.home) {
+            if (itemID == R.id.home) {
                 replaceFragment(new MainMenuFragment());
+
+            } else if (itemID == R.id.lectures) {
+                replaceFragment(new LectureListFragment());
 
             } else if (itemID == R.id.tests) {
                 replaceFragment(new TestListFragment());
@@ -41,13 +43,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
             return true;
-        });
-
-
-
-
-
-
+        });*/
 
         /*button = findViewById(R.id.buttonSwap);
         button.setOnClickListener(new View.OnClickListener() {
@@ -58,13 +54,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });*/
 
+        viewPager = findViewById(R.id.viewPager);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(this);
+        viewPager.setAdapter(adapter);
+
+
     }
 
-    private void replaceFragment(Fragment fragment) {
+    /*private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frameLayout, fragment);
 
         fragmentTransaction.commit();
-    }
+    }*/
 }
