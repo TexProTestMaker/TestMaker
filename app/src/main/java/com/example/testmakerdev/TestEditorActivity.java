b089f4c795fe64c9205703405a91c7325e908ea9
Melkoten <muryginperm@gmail.com>
2025-05-19 23:23:28 +0500

    Реализована функция addQuestionClick -  создаёт объект типа Question, заполняет его полученными данными и добавляет в текущий тест.

--- app/src/main/java/com/example/testmakerdev/TestEditorActivity.java
+++ app/src/main/java/com/example/testmakerdev/TestEditorActivity.java
@@ -14,6 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class TestEditorActivity extends AppCompatActivity {
 
     private EditText testTitleEditText, curQuestionEditText, answer1EditText, answer2EditText, answer3EditText, answer4EditText;
+    private EditText corAnsEditText; //ещё не реализовано в лэйауте, на данном ПК нужно ещё чинить билд, разберусь в СПБ
 
     private Button exitEditorButton, addQuestionButton, changeTitleButton;
     private Test currentTest;
@@ -39,6 +40,43 @@ public class TestEditorActivity extends AppCompatActivity {
         testTitleEditText.setText("");
     }
 
+    private void addQuestionClick() {
+        Question quest = new Question();
+        String question = curQuestionEditText.getText().toString();
+        String answer1 = answer1EditText.getText().toString();
+        String answer2 = answer2EditText.getText().toString();
+        String answer3 = answer3EditText.getText().toString();
+        String answer4 = answer4EditText.getText().toString();
+        String corAns = corAnsEditText.getText().toString();
+        List<String> list = new ArrayList<String>();
+
+        if (answer1.length() > 0) {
+            list.add(answer1);
+        }
+        if (answer2.length() > 0) {
+            list.add(answer2);
+        }
+        if (answer3.length() > 0) {
+            list.add(answer3);
+        }
+        if (answer4.length() > 0) {
+            list.add(answer4);
+        }
+
+        int number = -1;
+        try {
+             number = Integer.parseInt(corAns);
+        } catch (NumberFormatException e) {
+            Toast.makeText(this, "Ошибка: введите число!", Toast.LENGTH_SHORT).show();
+        }
+
+        if ((list.size() == 4) && (number != -1) && (question.length() > 0)) {
+            quest.answers = list;
+            quest.correctAnswer = number;
+            quest.questionText = question;
+            currentTest.addQuestion(quest);
+        }
+    }
     private void exitEditor() {
         Intent intent = new Intent(TestActivity.this, TestListFragment.class);
         startActivity(intent);
