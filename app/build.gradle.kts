0cd848b7c8c955dbbd818ed1d7a552ce5b2603e6
Tihon99 <tihon_baikal_99@mail.ru>
2025-05-20 00:23:43 +0300

    task70 edit build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,12 +37,12 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation ("com.github.mhiew:android-pdf-viewer:3.2.0-beta.3")
+    implementation (libs.android.pdf.viewer)
+    implementation (libs.viewpager2)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
