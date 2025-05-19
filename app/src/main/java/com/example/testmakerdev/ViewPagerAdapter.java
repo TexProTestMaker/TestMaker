153b3780d617944214aa5a2906da69d2fff88d50
Tihon99 <tihon_baikal_99@mail.ru>
2025-05-20 00:21:18 +0300

    task68 edit ViewPagerAdapter.java

--- app/src/main/java/com/example/testmakerdev/ViewPagerAdapter.java
+++ app/src/main/java/com/example/testmakerdev/ViewPagerAdapter.java
@@ -6,19 +6,22 @@ import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 public class ViewPagerAdapter extends FragmentStateAdapter {
-
-    public ViewPagerAdapter(FragmentActivity fa) {
-        super(fa);
+    public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
     }
 
     @NonNull
     @Override
     public Fragment createFragment(int position) {
         switch (position) {
-            case 0: return new MainMenuFragment();
-            case 1: return new LectureListFragment();
-            case 2: return new TestListFragment();
-            default: return new MainMenuFragment();
+            case 0:
+                return new MainMenuFragment();
+            case 1:
+                return new LectureListFragment();
+            case 2:
+                return new TestListFragment();
+            default:
+                return new MainMenuFragment();
         }
     }
 
@@ -27,4 +30,3 @@ public class ViewPagerAdapter extends FragmentStateAdapter {
         return 3;
     }
 }
-
