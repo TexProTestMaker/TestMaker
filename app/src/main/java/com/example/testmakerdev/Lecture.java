36984169cbb27a91a7aeb964a40756cd3326d3a8
ELisitskaya <lisitskaya.ea@edu.spbstu.ru>
2025-05-22 19:30:32 +0300

    -

---
+++ app/src/main/java/com/example/testmakerdev/Lecture.java
@@ -0,0 +1,19 @@
+package com.example.testmakerdev;
+
+public class Lecture {
+    private String name;
+    private String path;
+
+    public Lecture(String name, String path) {
+        this.name = name;
+        this.path = path;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+}
